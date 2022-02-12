ef761efc2109befbd2ba37e6225c7b0dbbf21e2b
qjl <qjl.com>
2022-02-12 10:05:59 +0800

    first commit

---
+++ src/java1/gitteat.java
@@ -0,0 +1,11 @@
+package java1;
+
+/**
+ * @author shkstart
+ * @create 2022-02-12 9:58
+ */
+public class gitteat {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
